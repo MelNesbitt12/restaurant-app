6da388d0899f1ddb8ed144bfebe9a157ea9ca347
MelNesbitt12 <mel.nalband@gmail.com>
2020-10-14 07:41:50 -0400

    Updates

---
+++ precache-manifest.41888cd699eb6a53293ad90d40c445ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03b2a45c688228daef6c2d1a5c98515",
+    "url": "/restaurant-app/index.html"
+  },
+  {
+    "revision": "63a263c27f0103c41832",
+    "url": "/restaurant-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76deefb03fd6ccb77886",
+    "url": "/restaurant-app/static/js/2.d34841c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant-app/static/js/2.d34841c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a263c27f0103c41832",
+    "url": "/restaurant-app/static/js/main.4fce696a.chunk.js"
+  },
+  {
+    "revision": "ae6bd2fda709f4bd2890",
+    "url": "/restaurant-app/static/js/runtime-main.c384e706.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/restaurant-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
